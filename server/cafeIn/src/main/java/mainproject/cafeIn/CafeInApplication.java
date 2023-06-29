91113c88ef40add0266c358628d7396c3e9f6f7a
[Hayan] <[c_white723@naver.com]>
2023-06-29 22:44:24 +0900

    � init: 스프링 부트 프로젝트 초기 셋팅 (2023.06.29 조하얀)

---
+++ server/cafeIn/src/main/java/mainproject/cafeIn/CafeInApplication.java
@@ -0,0 +1,12 @@
+package mainproject.cafeIn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeInApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CafeInApplication.class, args);
+	}
+
+}
---
+++ server/cafeIn/src/test/java/mainproject/cafeIn/CafeInApplicationTests.java
@@ -0,0 +1,13 @@
+package mainproject.cafeIn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeInApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
