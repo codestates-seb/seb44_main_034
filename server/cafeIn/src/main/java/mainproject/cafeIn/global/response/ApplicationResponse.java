6e45b3d8f18c16d13a1a6b3e1642a4a924b04d74
[Hayan] <[c_white723@naver.com]>
2023-07-03 09:17:42 +0900

    ✨ feat: Application Response 개발 (2023.07.03 조하얀)

---
+++ server/cafeIn/src/main/java/mainproject/cafeIn/global/response/ApplicationResponse.java
@@ -0,0 +1,27 @@
+package mainproject.cafeIn.global.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.Data;
+
+@Data
+@JsonPropertyOrder({"message", "payload"})
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ApplicationResponse<T> {
+    private final static String SUCCESS_MESSAGE = "성공적으로 요청을 불러왔습니다.";
+    private T payload;
+    private String message;
+
+    public ApplicationResponse(T payload) {
+        this.payload = payload;
+        this.message = SUCCESS_MESSAGE;
+    }
+
+    public ApplicationResponse() {
+        this.message = SUCCESS_MESSAGE;
+    }
+
+    public ApplicationResponse(String message) {
+        this.message = message;
+    }
+}
