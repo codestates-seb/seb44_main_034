9dade294fe181ab7479d1dd977044ab74142dbf8
baejb <bjb6478@naver.com>
2023-07-04 17:36:14 +0900

    chore: tanstack query라이브러리설치

--- client/src/common/common.tsx
+++ client/src/common/common.tsx
@@ -1,4 +1,14 @@
-export const COLOR_1 : {green:string, light_green:string, ivory:string, sand:string, dark_sand:string, brown:string, dark_brown:string, black:string, white:string} ={
+export const COLOR_1: {
+  green: string;
+  light_green: string;
+  ivory: string;
+  sand: string;
+  dark_sand: string;
+  brown: string;
+  dark_brown: string;
+  black: string;
+  white: string;
+} = {
   green: '#CED5B2',
   light_green: '#EAEDCC',
   ivory: '#FDFAE3',
@@ -7,9 +17,22 @@ export const COLOR_1 : {green:string, light_green:string, ivory:string, sand:str
   brown: '#6C574E',
   dark_brown: '#483219',
   black: '#000000',
-  white: '#FFFFFF'
-}
-export const FONT_SIZE_1 : {normal_1:string, normal_2:string, normal_3:string, small_1:string, small_2:string, small_3:string, big_1:string, big_2:string, big_3:string, big_4:string, big_5:string, big_6:string} ={
+  white: '#FFFFFF',
+};
+export const FONT_SIZE_1: {
+  normal_1: string;
+  normal_2: string;
+  normal_3: string;
+  small_1: string;
+  small_2: string;
+  small_3: string;
+  big_1: string;
+  big_2: string;
+  big_3: string;
+  big_4: string;
+  big_5: string;
+  big_6: string;
+} = {
   normal_1: '14px',
   normal_2: '16px',
   normal_3: '18px',
@@ -21,9 +44,26 @@ export const FONT_SIZE_1 : {normal_1:string, normal_2:string, normal_3:string, s
   big_3: '28px',
   big_4: '32px',
   big_5: '36px',
-  big_6: '40px'
-}
-export const FONT_SIZE_2 : {normal_1:string, normal_2:string, normal_3:string, normal_4:string, normal_5:string, normal_6:string, small_1:string, small_2:string, small_3:string, big_1:string, big_2:string, big_3:string, big_4:string, big_5:string, big_6:string, big_7:string} ={
+  big_6: '40px',
+};
+export const FONT_SIZE_2: {
+  normal_1: string;
+  normal_2: string;
+  normal_3: string;
+  normal_4: string;
+  normal_5: string;
+  normal_6: string;
+  small_1: string;
+  small_2: string;
+  small_3: string;
+  big_1: string;
+  big_2: string;
+  big_3: string;
+  big_4: string;
+  big_5: string;
+  big_6: string;
+  big_7: string;
+} = {
   normal_1: '0.5rem',
   normal_2: '0.8rem',
   normal_3: '1rem',
@@ -40,8 +80,17 @@ export const FONT_SIZE_2 : {normal_1:string, normal_2:string, normal_3:string, n
   big_5: '2.8em',
   big_6: '2.8rem',
   big_7: '3rem',
-}
-export const FONT_WEIGHT : {weight_100:string, weight_200:string, weight_300:string, weight_400:string, weight_500:string, weight_600:string, weight_700:string, weight_800:string} ={
+};
+export const FONT_WEIGHT: {
+  weight_100: string;
+  weight_200: string;
+  weight_300: string;
+  weight_400: string;
+  weight_500: string;
+  weight_600: string;
+  weight_700: string;
+  weight_800: string;
+} = {
   weight_100: '100',
   weight_200: '200',
   weight_300: '300',
@@ -49,5 +98,5 @@ export const FONT_WEIGHT : {weight_100:string, weight_200:string, weight_300:str
   weight_500: '500',
   weight_600: '600',
   weight_700: '700',
-  weight_800: '800'
-}
+  weight_800: '800',
+};
