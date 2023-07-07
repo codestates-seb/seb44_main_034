02b5189bae87c4eb68a7aca4efeee10f6563fa31
hellok09 <hellok0915@gmail.com>
2023-07-07 17:13:32 +0900

    ✨ feat: member entity (2023.07.07 김현희)

--- server/cafeIn/src/main/java/mainproject/cafeIn/domain/member/entity/d.java
+++
@@ -1,4 +0,0 @@
-package mainproject.cafeIn.domain.member.entity;
-
-public class d {
-}
---
+++ server/cafeIn/src/main/java/mainproject/cafeIn/domain/member/entity/enums/MemberGrade.java
@@ -0,0 +1,17 @@
+package mainproject.cafeIn.domain.member.entity.enums;
+
+import lombok.Getter;
+
+public enum MemberGrade {
+
+    GRADE_GREEN_BEAN("생두"),
+    GRADE_ROASTED_BEAN("원두"),
+    GRADE_ESPRESSO("에스프레소");
+
+    @Getter
+    private String grade;
+
+    MemberGrade(String grade){
+        this.grade = grade;
+    }
+}
