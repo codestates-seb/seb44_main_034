add6e3aff8956e9d281541faab52e2367c6cda93
cwhite723 <c_white723@naver.com>
2023-07-12 11:09:39 +0900

    ✨ feat: CafeBookmark Entity, 비즈니스 로직 개발 (2023.07.12 조하얀)

--- server/cafeIn/src/main/java/mainproject/cafeIn/domain/cafe/controller/CafeController.java
+++ server/cafeIn/src/main/java/mainproject/cafeIn/domain/cafe/controller/CafeController.java
@@ -6,6 +6,7 @@ import mainproject.cafeIn.domain.cafe.dto.request.PageCafeRequest;
 import mainproject.cafeIn.domain.cafe.dto.request.SearchCafeFilterCondition;
 import mainproject.cafeIn.domain.cafe.dto.response.GetCafeDetailResponse;
 import mainproject.cafeIn.domain.cafe.dto.response.GetCafesResponse;
+import mainproject.cafeIn.domain.cafe.service.CafeBookmarkService;
 import mainproject.cafeIn.domain.cafe.service.CafeService;
 import mainproject.cafeIn.global.auth.interceptor.JwtParseInterceptor;
 import mainproject.cafeIn.global.response.ApplicationResponse;
@@ -15,15 +16,18 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
+import static org.springframework.http.HttpStatus.*;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/cafes")
 public class CafeController {
     private final CafeService cafeService;
+    private final CafeBookmarkService cafeBookmarkService;
 
     // 카페 등록
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
+    @ResponseStatus(CREATED)
     public ApplicationResponse<Long> createCafe(@RequestPart(value = "dto") CafeInfoRequest request,
                                                 @RequestPart(value = "cafeImage", required = false) MultipartFile multipartFile) {
         Long loginId = JwtParseInterceptor.getAuthenticatedUserId();
@@ -34,19 +38,19 @@ public class CafeController {
 
     // 카페 수정
     @PatchMapping("/{cafe-id}")
-    @ResponseStatus(HttpStatus.OK)
+    @ResponseStatus(OK)
     public ApplicationResponse updateCafe(@PathVariable("cafe-id") Long cafeId,
                                           @RequestPart(value = "dto") CafeInfoRequest request,
                                           @RequestPart(value = "cafeImage", required = false) MultipartFile multipartFile) {
         Long loginId = JwtParseInterceptor.getAuthenticatedUserId();
         cafeService.updateCafe(loginId, cafeId, request, multipartFile);
 
-        return new ApplicationResponse();
+        return new ApplicationResponse<>();
     }
 
     // 카페 상세 조회
     @GetMapping("/{cafe-id}")
-    @ResponseStatus(HttpStatus.OK)
+    @ResponseStatus(OK)
     public ApplicationResponse<GetCafeDetailResponse> getCafe(@PathVariable("cafe-id") Long cafeId) {
 
         return new ApplicationResponse<>();
@@ -54,17 +58,18 @@ public class CafeController {
 
     // 카페 삭제
     @DeleteMapping("/{cafe-id}")
-    @ResponseStatus(HttpStatus.OK)
+    @ResponseStatus(OK)
     public ApplicationResponse deleteCafe(@PathVariable("cafe-id") Long cafeId,
                                           @RequestBody String password) {
         Long loginId = JwtParseInterceptor.getAuthenticatedUserId();
         cafeService.deleteCafe(loginId, cafeId, password);
 
-        return new ApplicationResponse();
+        return new ApplicationResponse<>();
     }
 
     // 카페 리스트 조회
     @GetMapping
+    @ResponseStatus(OK)
     public ApplicationResponse<List<GetCafesResponse>> getCafes(SearchCafeFilterCondition searchCafeFilterCondition,
                                                                 PageCafeRequest pageCafeRequest) {
         List<GetCafesResponse> response = cafeService.searchCafesByFilterCondition(searchCafeFilterCondition, pageCafeRequest.of());
@@ -73,6 +78,14 @@ public class CafeController {
     }
 
     // 카페 북마크
+    @PostMapping("/{cafe-id}/Bookmark")
+    @ResponseStatus(OK)
+    public ApplicationResponse bookmarkCafe(@PathVariable("cafe-id") Long cafeId) {
+        Long loginId = JwtParseInterceptor.getAuthenticatedUserId();
+        cafeBookmarkService.bookmarkCafe(cafeId, loginId);
+
+        return new ApplicationResponse<>();
+    }
 
     // 카페 정보 조회
 
---
+++ server/cafeIn/src/main/java/mainproject/cafeIn/domain/cafe/service/CafeBookmarkService.java
@@ -0,0 +1,33 @@
+package mainproject.cafeIn.domain.cafe.service;
+
+import lombok.RequiredArgsConstructor;
+import mainproject.cafeIn.domain.cafe.entity.Cafe;
+import mainproject.cafeIn.domain.cafe.entity.CafeBookmark;
+import mainproject.cafeIn.domain.cafe.repository.CafeBookmarkRepository;
+import mainproject.cafeIn.domain.member.entity.Member;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class CafeBookmarkService {
+    private final CafeService cafeService;
+    private final MemberService memberService;
+    private final CafeBookmarkRepository cafeBookmarkRepository;
+
+    @Transactional
+    public void bookmarkCafe(Long cafeId, Long loginId) {
+        Cafe cafe = cafeService.findCafeById(cafeId);
+        Member member = memberService.findMemberById(loginId);
+
+        Optional<Long> cafeBookmarkId = cafeBookmarkRepository.findCafeBookmarkByCafeIdAndMemberId(cafeId, loginId);
+        if (cafeBookmarkId.isPresent()) {
+            cafeBookmarkRepository.deleteById(cafeBookmarkId.get());
+        } else {
+            cafeBookmarkRepository.save(CafeBookmark.of(cafe, member));
+        }
+    }
+}
