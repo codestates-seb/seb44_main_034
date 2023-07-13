92ed493065d305700f3ff5bd2be10a035d2c63e3
NOGSUNGYOON <nsy1996@naver.com>
2023-07-13 15:21:13 +0900

    ♻ refactor: 로그인 수정 (2023.07.13 노성윤)

--- server/cafeIn/src/main/java/mainproject/cafeIn/global/auth/userdetails/UsersDetailsService.java
+++ server/cafeIn/src/main/java/mainproject/cafeIn/global/auth/userdetails/UsersDetailsService.java
@@ -1,6 +1,11 @@
 package mainproject.cafeIn.global.auth.userdetails;
 
+import lombok.Builder;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import mainproject.cafeIn.domain.member.entity.Member;
+import mainproject.cafeIn.domain.member.entity.enums.MemberStatus;
+import mainproject.cafeIn.domain.member.repository.MemberRepository;
 import mainproject.cafeIn.domain.owner.entity.Owner;
 import mainproject.cafeIn.domain.owner.enums.OwnerStatus;
 import mainproject.cafeIn.domain.owner.repository.OwnerRepository;
@@ -20,21 +25,22 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class UsersDetailsService implements UserDetailsService { // 사용자 크리덴셜 조회한 후 AuthenticationManager에게 전달
     private final OwnerRepository ownerRepository;
+    private final MemberRepository memberRepository;
     private final CustomAuthorityUtils authorityUtils;
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Optional<Owner> optionalOwner = ownerRepository.findByEmail(username);
-        Owner findOwner = optionalOwner.orElseThrow(() -> new CustomException(ErrorCode.MEMBER_NOT_FOUND));
+        Owner owner = optionalOwner.orElse(null);
 
-        //TODO MemberStatus 조건 추가
-        try {
-            if (findOwner.getOwnerStatus() == OwnerStatus.OWNER_QUIT) {
-                throw new CustomException(ErrorCode.MEMBER_NOT_FOUND);
-            }
-        } catch (Exception e) {
-            throw new CustomException(ErrorCode.MEMBER_NOT_FOUND);
+        Optional<Member> optionalMember = memberRepository.findByEmail(username);
+        Member member = optionalMember.orElse(null);
+
+        if (owner != null && owner.getOwnerStatus() != OwnerStatus.OWNER_QUIT) {
+            return new OwnerDetails(owner);
+        } else if (member != null && member.getStatus() != MemberStatus.MEMBER_QUIT) {
+            return new MemberDetails(member);
         }
-        return new OwnerDetails(findOwner);
+        throw new CustomException(ErrorCode.MEMBER_NOT_FOUND);
     }
 
     private final class OwnerDetails extends Owner implements UserDetails {
@@ -76,5 +82,44 @@ public class UsersDetailsService implements UserDetailsService { // 사용자 �
             return true;
         }
     }
-    // TODO MemberDetails 구현
+
+    private final class MemberDetails extends Member implements UserDetails {
+        MemberDetails(Member member) {
+            setId(member.getId());
+            setDisplayName(member.getDisplayName());
+            setEmail(member.getEmail());
+            setPassword(member.getPassword());
+            setRoles(member.getRoles());
+        }
+
+        @Override
+        public Collection<? extends GrantedAuthority> getAuthorities() {
+            return authorityUtils.createAuthorities(this.getRoles());
+        }
+
+        @Override
+        public String getUsername() {
+            return getEmail();
+        }
+
+        @Override
+        public boolean isAccountNonExpired() {
+            return true;
+        }
+
+        @Override
+        public boolean isAccountNonLocked() {
+            return true;
+        }
+
+        @Override
+        public boolean isCredentialsNonExpired() {
+            return true;
+        }
+
+        @Override
+        public boolean isEnabled() {
+            return true;
+        }
+    }
 }
