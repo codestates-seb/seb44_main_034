a6ed754de8b271d87057c6d7c270b665f03726ee
baejb <bjb6478@naver.com>
2023-07-17 10:26:50 +0900

    feat: 카페 상세정보페이지 post 제외 구현

--- client/src/pages/CafePage.tsx
+++ client/src/pages/CafePage.tsx
@@ -1,9 +1,78 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import { styled } from 'styled-components';
-import { FONT_SIZE_2 } from '../common/common';
-import { COLOR_1 } from '../common/common';
-
+import { FONT_SIZE_1, COLOR_1 } from '../common/common';
+import CafeDetailMenu from '../components/cafe/CafeDetailMenu';
+import CafeDetailsInfo from '../components/cafe/CafeDetailsInfo';
+import Loading from '../components/Loading';
+import { CafeDetailType, MenuDataType } from '../types/type';
+import { baseURL } from '../common/baseURL';
+import { useParams } from 'react-router-dom';
 const CafePage = () => {
-  return <></>;
-};
+  const [cafeDetail, setCafeDetail] = useState<CafeDetailType | undefined>();
+  const [menus, setMenus] = useState<MenuDataType[][] | undefined>();
+  const [isLoading, setIsLoading] = useState(true);
+  const { id } = useParams();
+  useEffect(() => {
+    const fetchCafeData = async () => {
+      //지금은 목업으로 해서 data[0]으로 했지만 추후 수정
+      try {
+        const response = await axios.get('http://localhost:3000/cafes'); // 서버 API 엔드포인트에 맞게 수정
+        // const response = await axios.get(`${baseURL}/cafes/${id}`); 오너마이페이지에서 id받아올것
+        const data = response.data[0];
 
+        setCafeDetail(data.cafeDetail);
+        setMenus(data.menus);
+        console.log(data.menus);
+        setIsLoading(false);
+      } catch (error) {
+        console.error('Error fetching cafe data:', error);
+      }
+    };
+    fetchCafeData();
+  }, []);
+
+  return (
+    <>
+      {isLoading ? (
+        <Loading /> // 로딩 페이지 표시
+      ) : (
+        <S.Container>
+          {cafeDetail && <CafeDetailsInfo cafeDetail={cafeDetail} />}
+          <S.Title>
+            Menu
+            <div></div>
+          </S.Title>
+          {menus && <CafeDetailMenu menu={menus} />}
+          <S.Title>
+            Post
+            <div></div>
+          </S.Title>
+        </S.Container>
+      )}
+    </>
+  );
+};
+const S = {
+  Container: styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center; /* 수평 가운데 정렬 */
+    flex-direction: column;
+    overflow: scroll;
+    @media screen and (max-width: 767px) {
+      width: 100%;
+      flex-direction: column;
+      justify-content: flex-start;
+    }
+  `,
+  Title: styled.div`
+    font-size: ${FONT_SIZE_1.big_6};
+    margin-top: 2%;
+    > div {
+      border-bottom: 1px solid ${COLOR_1.green};
+    }
+  `,
+};
 export default CafePage;
--- client/src/types/type.ts
+++ client/src/types/type.ts
@@ -6,26 +6,53 @@ export interface CafePostList {
 }
 
 export interface PostData {
-  postId?:number;
-  cafeId: number|string;
-  cafeName:string;
+  postId?: number;
+  cafeId: number | string;
+  cafeName: string;
   title: string;
   createdAt: string;
   updatedAt: string;
-  authorId: number|string;
+  authorId: number | string;
   author: string;
-  image: string|File;
+  image: string | File;
   content: string;
-  starRating: number|undefined;
+  starRating: number | undefined;
   isBookmarked: boolean;
-  tag?:string[];
+  tag?: string[];
   comment?: PostComment[];
 }
 
 export interface PostComment {
-  authorId: number|string;
+  authorId: number | string;
   author: string;
   body: string;
   createdAt: string;
   updatedAt: string;
 }
+
+export type CafeDetailType = {
+  ownerId: number;
+  cafeId: number;
+  name: string;
+  address: string;
+  latitude: number;
+  longitude: number;
+  contact: string;
+  notice: string;
+  image: string | null;
+  rating: number;
+  openTime: string;
+  closeTime: string;
+  hasParking: boolean;
+  hasDessert: boolean;
+  openAllTime: boolean;
+  chargingAvailable: boolean;
+  petFriendly: boolean;
+};
+
+export type MenuDataType = {
+  menuId: number;
+  name: string;
+  price: number;
+  menuType: string;
+};
