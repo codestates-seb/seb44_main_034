619fe06e27e68342151b7ec73de1648539773789
cwhite723 <c_white723@naver.com>
2023-07-17 21:39:27 +0900

    ✨ feat: image 수정, 삭제 개발 (2023.07.17 조하얀)

--- server/src/main/java/mainproject/cafeIn/global/cloud/S3ImageService.java
+++ server/src/main/java/mainproject/cafeIn/global/cloud/S3ImageService.java
@@ -2,6 +2,7 @@ package mainproject.cafeIn.global.cloud;
 
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -39,6 +40,22 @@ public class S3ImageService {
         removeNewFile(uploadFile);
 
         return uploadImageUrl;
+    }
+
+    public String update(String imageUrl, MultipartFile multipartFile, String dirName) throws IOException {
+        delete(imageUrl);
+
+        return upload(multipartFile, dirName);
+    }
+
+    public void delete(String imageUrl) {
+        String fileName = extractFileNameFromUrl(imageUrl);
+        amazonS3Client.deleteObject(new DeleteObjectRequest(bucket, fileName));
+    }
+
+    private String extractFileNameFromUrl(String imageUrl) {
+
+        return imageUrl.substring(imageUrl.lastIndexOf("/"));
     }
 
     private String putS3(File uploadFile, String fileName) {
