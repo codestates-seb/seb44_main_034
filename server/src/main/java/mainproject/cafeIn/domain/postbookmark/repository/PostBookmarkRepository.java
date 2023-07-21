cb8084d651906281731395329bcbbce5d990376b
RYEOL-KIM <rlaemrfuf043@gmail.com>
2023-07-21 13:28:42 +0900

    fix: 리포지터리 메서드명 수정(23.07.21 김득렬)

--- server/src/main/java/mainproject/cafeIn/domain/postbookmark/repository/PostBookmarkRepository.java
+++ server/src/main/java/mainproject/cafeIn/domain/postbookmark/repository/PostBookmarkRepository.java
@@ -1,7 +1,6 @@
 package mainproject.cafeIn.domain.postbookmark.repository;
 
 import mainproject.cafeIn.domain.postbookmark.entity.PostBookmark;
-import mainproject.cafeIn.domain.tag.entity.PostTag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -10,6 +9,6 @@ import java.util.Optional;
 public interface PostBookmarkRepository extends JpaRepository<PostBookmark, Long> {
     Optional<PostBookmark> findByMemberIdAndPostPostId(Long memberId, Long postId);
 
-    Optional<List<PostBookmark>> findPostBookmarkByMemberMemberId(Long memberId);
+    Optional<List<PostBookmark>> findPostBookmarksByMemberId(Long memberId);
 }
 
