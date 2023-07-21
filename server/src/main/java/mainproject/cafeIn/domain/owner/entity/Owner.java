3a5748f512f61282dbd5acaea22be63075c81ade
RYEOL-KIM <rlaemrfuf043@gmail.com>
2023-07-21 16:53:05 +0900

    fix: sql 관련 수정

--- server/src/main/java/mainproject/cafeIn/domain/owner/entity/Owner.java
+++ server/src/main/java/mainproject/cafeIn/domain/owner/entity/Owner.java
@@ -14,7 +14,6 @@ import java.util.List;
 @Getter
 @Setter // UserDetails 만드는데 필요
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Table(name = "owner")
 public class Owner extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
