25dc3a5700a01c71d64e522137d153f31b44139c
baejb <bjb6478@naver.com>
2023-07-22 20:21:18 +0900

    feat : 이미지 null 처리

--- client/src/components/cafe/EditCafeInfo.tsx
+++ client/src/components/cafe/EditCafeInfo.tsx
@@ -38,7 +38,7 @@ const EditCafeInfo = ({ cafeId }: { cafeId: string | undefined }) => {
     isPetFriendly: false,
     hasDessert: false,
   });
-  const [imageFile, setImageFile] = useState<null | string | Blob>("");
+  const [imageFile, setImageFile] = useState<null | string | Blob>(null);
   const [previewImage, setPreviewImage] = useState<string | null>("");
 
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -155,6 +155,8 @@ const EditCafeInfo = ({ cafeId }: { cafeId: string | undefined }) => {
     // if (imageFile) {
     if (imageFile) {
       formData.append("cafeImage", imageFile); //이미지 수정 안했을 때 이미지 그대로 다시 해야되는거 수정
+    } else {
+      formData.append("cafeImage", null);
     }
     const json = JSON.stringify(editData);
     const info = new Blob([json], { type: "application/json" });
