050f94653f0a7447fe22fc821eba2b4453587a0c
baejb <bjb6478@naver.com>
2023-07-22 19:40:48 +0900

    feat : 이미지 null

--- client/src/components/cafe/EditCafeInfo.tsx
+++ client/src/components/cafe/EditCafeInfo.tsx
@@ -38,7 +38,7 @@ const EditCafeInfo = ({ cafeId }: { cafeId: string | undefined }) => {
     isPetFriendly: false,
     hasDessert: false,
   });
-  const [imageFile, setImageFile] = useState<string | Blob>("");
+  const [imageFile, setImageFile] = useState<null | string | Blob>("");
   const [previewImage, setPreviewImage] = useState<string | null>("");
 
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -153,8 +153,9 @@ const EditCafeInfo = ({ cafeId }: { cafeId: string | undefined }) => {
 
     const formData = new FormData();
     // if (imageFile) {
-    formData.append("cafeImage", imageFile); //이미지 수정 안했을 때 이미지 그대로 다시 해야되는거 수정
-    // }
+    if (imageFile) {
+      formData.append("cafeImage", imageFile); //이미지 수정 안했을 때 이미지 그대로 다시 해야되는거 수정
+    }
     const json = JSON.stringify(editData);
     const info = new Blob([json], { type: "application/json" });
     formData.append("dto", info);
