204c9cd808200c7a6e97a1f7bc673f8f8742c701
yuheejoon <yuhetube4@gmail.com>
2023-07-23 19:28:32 +0900

    fix ref

--- client/src/components/users/EditUserMyPageBox.tsx
+++ client/src/components/users/EditUserMyPageBox.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
 import axios from "axios";
 import DeleteAccountBox from "../deleteaccoutbox/DeleteAccoutBox";
