d5b5ea9b33b455e1efcc29d143e4c8005b7f176c
RYEOL-KIM <rlaemrfuf043@gmail.com>
2023-07-24 03:59:11 +0900

    refactor: grade 정보 추가

--- server/src/main/java/mainproject/cafeIn/domain/comment/dto/response/ReplyResponse.java
+++ server/src/main/java/mainproject/cafeIn/domain/comment/dto/response/ReplyResponse.java
@@ -15,14 +15,16 @@ public class ReplyResponse {
     private Long replyId;
     private Long authorId;
     private String author;
+    private String grade;
     private String content;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 
-    public ReplyResponse(Long replyId, Long authorId, String author, String content, LocalDateTime createdAt, LocalDateTime updatedAt) {
+    public ReplyResponse(Long replyId, Long authorId, String author, String grade, String content, LocalDateTime createdAt, LocalDateTime updatedAt) {
         this.replyId = replyId;
         this.authorId = authorId;
         this.author = author;
+        this.grade = grade;
         this.content = content;
         this.createdAt = createdAt;
         this.updatedAt = updatedAt;
