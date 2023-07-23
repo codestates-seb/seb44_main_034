15b08e99e12e04edae35f6c57cf8be1d4b740c48
sogood17 <sogoodpiano@gmail.com>
2023-07-23 23:27:22 +0900

    배포 오류 수정

--- client/src/components/post/postItemHead.tsx
+++ client/src/components/post/postItemHead.tsx
@@ -9,7 +9,7 @@ import { ReqPostData } from "../../types/type";
 import { ResPostData } from "../../types/type";
 import { PostItemAtom } from "../../recoil/postState";
 import { PostImgAtom } from "../../recoil/postState";
-import { GetPostAtom } from "../../recoil/postState";
+// import { GetPostAtom } from "../../recoil/postState";
 import { IoShareSocial } from "react-icons/io5";
 import { GoBookmark, GoBookmarkFill } from "react-icons/go";
 import { COLOR_1, FONT_SIZE_2, FONT_WEIGHT } from "../../common/common";
