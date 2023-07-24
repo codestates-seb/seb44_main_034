aace02b95392ce002c8dff15c9e51eaa7af96c82
sogood17 <sogoodpiano@gmail.com>
2023-07-24 23:40:51 +0900

    소셜 url 수정

--- client/src/common/KakaoShare.tsx
+++ client/src/common/KakaoShare.tsx
@@ -15,16 +15,16 @@ const KakaoShare = ({ title, image }: KakaoProps) => {
         imageUrl: image,
         link: {
           // [내 애플리케이션] > [플랫폼] 에서 등록한 사이트 도메인과 일치해야 함
-          mobileWebUrl: "https://cafein034.vercel.app/ ",
-          webUrl: "https://cafein034.vercel.app/ ",
+          mobileWebUrl: "https://cafein34.vercel.app/ ",
+          webUrl: "https://cafein34.vercel.app/ ",
         },
       },
       buttons: [
         {
           title: "카페인 방문하기",
           link: {
-            mobileWebUrl: "https://cafein034.vercel.app/ ",
-            webUrl: "https://cafein034.vercel.app/ ",
+            mobileWebUrl: "https://cafein34.vercel.app/ ",
+            webUrl: "https://cafein34.vercel.app/ ",
           },
         },
       ],
